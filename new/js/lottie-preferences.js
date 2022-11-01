3b7e323ad19f0d7c27b40ebef9921ff725490ca0
Luke Caish-Sadlek <64913461+Luke-Calek@users.noreply.github.com>
2022-11-01 19:51:59 +0100

    Version 2 2022

---
+++ new/js/lottie-preferences.js
@@ -0,0 +1,59 @@
+// Animation configuration
+
+document.addEventListener('DOMContentLoaded', function () {
+
+
+  // Innovation
+  LottieInteractivity.create({
+    player: '#innovation',
+    mode: 'scroll',
+    actions: [
+      {
+        visibility: [.2, .85],
+        type: 'seek',
+        frames: [0, 359],
+      },
+    ],
+  });
+
+
+  // Needs
+  LottieInteractivity.create({
+    player: '#needs',
+    mode: 'scroll',
+    actions: [
+      {
+        visibility: [0.25, 0.65],
+        type: 'seek',
+        frames: [0, 240],
+      },
+    ],
+  });
+
+
+  // Process
+  LottieInteractivity.create({
+    player: '#process-desktop',
+    mode: 'scroll',
+    actions: [
+      {
+        visibility: [0.2, 0.65],
+        type: 'seek',
+        frames: [0, 390],
+      },
+    ],
+  });
+  LottieInteractivity.create({
+    player: '#process-mobile',
+    mode: 'scroll',
+    actions: [
+      {
+        visibility: [0.2, 0.65],
+        type: 'seek',
+        frames: [0, 390],
+      },
+    ],
+  });
+
+
+});
